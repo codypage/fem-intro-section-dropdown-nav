37ff3680f4e6eb5c977988ced8f4af4c08a18fe5
codypage <100495823+codypage@users.noreply.github.com>
2023-05-02 11:42:53 -0500

    first commit/getting setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img alt="Workit logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    Features Company Careers About Login Register Make remote work Get your team in sync, no matter
+    your location. Streamline processes, create team rituals, and watch productivity soar. Learn
+    more
+  </main>
+</template>
+
+<style lang="scss"></style>
